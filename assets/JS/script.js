038d70c79a5989a18e75189055fc06c440b0ac42
JoeBA <135997793+JoeBA-1@users.noreply.github.com>
2023-08-15 23:12:42 +0300

    small modifications

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,23 @@
 document.title = 'Blackjack'
+let riddleEL = document.getElementById("riddle")
+let cardsEL = document.getElementById("cards")
+let sumEL = document.getElementById("sum")
+let startBtnEL = document.getElementById("start_btn")
+let newcardBtnEL = document.getElementById("newcard_btn")
+let newgameBtnEL = document.getElementById("newgame_btn")
+let amountEL = document.getElementById("amount")
+const cardnumbers = ["1","2","3","4","5","6","7","8","9","10","11","12"]
+
+newgameBtnEL.addEventListener("click", function(){
+
+window.location.replace("index.html")
+
+})
+
+newcardBtnEL.addEventListener("click", function(){
+    let char01 = Math.floor (Math.random(cardnumbers) * cardnumbers.length )
+    cardsEL.innerHTML += `${cardnumbers[char01]} `
+
+
+
+})
